b74a6d00cda56fc714f9cda506b4103010931313
Roberto Fortes <robertbernd23@gmail.com>
2021-06-10 23:55:25 -0300

    funcao cronometro

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,12 +4,24 @@ var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
+campo.on("input", function () {
     var conteudo = campo.val();
-    var conteudoSemEspaco = conteudo.replace(/\s+/g,'');
+    var conteudoSemEspaco = conteudo.replace(/\s+/g, '');
     var qtdPalavras = conteudo.split(/\S+/).length - 1;
     $("#contador-palavras").text(qtdPalavras);
 
     var qtdCaracteres = conteudoSemEspaco.length;
     $("#contador-caracteres").text(qtdCaracteres);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function () {
+    var cronometroID = setInterval(function () {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
 });
