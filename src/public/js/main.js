79c85fb21ea3d61ae8728dc9b2fb6d6d92ba270c
Hudson Miranda <hudsonfereira@hotmail.com>
2021-06-10 23:17:40 -0300

    contando atraves de evento

---
+++ src/public/js/main.js
@@ -0,0 +1,15 @@
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+    var conteudoSemEspaco = conteudo.replace(/\s+/g,'');
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudoSemEspaco.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
