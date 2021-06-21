ab74f4e3542322384cb2cb077094955a0f5c3ef9
Roberto Fortes <robertbernd23@gmail.com>
2021-06-21 18:24:19 -0300

    Botao deletar e design em placar

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -90,3 +90,32 @@ function inserePlacar(){
 
     corpoTabela.append(linha);
 }
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Roberto"
+    var numPalavras = $("#contador-palavras").text();
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+    corpoTabela.append(linha);
+   }
+
+   function novaLinha(usuario, palavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("materialicons").text("delete");
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+    return linha;
+   }
+   
+   function removeLinha(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+   }
